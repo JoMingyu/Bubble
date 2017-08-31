1bd7098c5c2a012b9ed152b9e3f5716a013145cf
Yun Jung Hyeon <yjh54240@naver.com>
2017-08-31 20:29:52 +0900

    툴바 추가 액티비티, 프래그먼트, 모델 패키지 추가

--- Binu/app/src/main/java/com/bubble/swcamp/android/activities/MainActivity.java
+++ Binu/app/src/main/java/com/bubble/swcamp/android/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bubble.swcamp.android;
+package com.bubble.swcamp.android.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bubble.swcamp.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
