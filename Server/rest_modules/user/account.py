023fba2bb3ff6401e418081cb635d6077d61615b
PlanB <city7310@naver.com>
2017-09-01 00:45:57 +0900

    계정 관련 주요 로직 변경
    회원가입 : 자체 회원가입만 해당
    로그인 : sns 연결 후 이메일 받아오고, 테이블에 없으면 insertr(자동 회원가입)

--- Server/rest_modules/user/account.py
+++ Server/rest_modules/user/account.py
@@ -7,42 +7,54 @@ from support.mysql import query
 
 class Signup(Resource):
     def post(self):
-        # 회원가입
+        # 자체 회원가입
+        email = request.form['email']
         _id = request.form['id']
         pw = request.form['pw']
         nickname = request.form['nickname']
+        gender = request.form['gender']
 
-        res = query("SELECT * FROM account WHERE id='{0}'".format(_id))
+        res = query("SELECT * FROM account WHERE email='{0}' OR id='{1}'".format(email, _id))
         if res:
             return '', 204
         else:
-            query("INSERT INTO account VALUES('{0}', '{1}', '{2}')".format(_id, pw, nickname))
+            query("INSERT INTO account VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(email, _id, pw, nickname, gender))
             return '', 201
 
 
 class SignIn(Resource):
     def post(self):
         # 로그인
-        _id = request.form['id']
-        pw = request.form['pw']
+        if request.form['sns'].upper() == 'TRUE':
+            email = request.form['email']
+            if not query("SELECT * FROM account_sns WHERE email='{0}'".format(email)):
+                # 가입 데이터가 없으면
+                nickname = request.form['nickname']
+                gender = request.form['gender']
 
-        res = query("SELECT * FROM account WHERE id='{0}' AND pw='{1}'".format(_id, pw))
-        if res:
+                query("INSERT INTO account_sns VALUES('{0}', '{1}', '{2}')".format(email, nickname, gender))
+
             return '', 200
         else:
-            return '', 204
+            _id = request.form['id']
+            pw = request.form['pw']
+
+            if query("SELECT * FROM account WHERE id='{0}' AND pw='{1}'".format(_id, pw)):
+                return '', 200
+            else:
+                return '', 204
 
 
 class FindPassword(Resource):
     def post(self):
         # 비밀번호 찾기
-        _id = request.form['id']
+        email = request.form['email']
 
-        res = query("SELECT * FROM account WHERE id='{0}'".format(_id))
+        res = query("SELECT * FROM account WHERE email='{0}'".format(email))
         if res:
             # id 존재함
             new_pw = str(uuid.uuid4())[:8]
-            query("UPDATE account SET pw='{0}' WHERE id='{1}'".format(new_pw, _id))
+            query("UPDATE account SET pw='{0}' WHERE email='{1}'".format(new_pw, email))
 
             return new_pw, 200
 
