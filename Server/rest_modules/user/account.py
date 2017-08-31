c445cc8d8af16c056ef56d9e3f79458f1cf3a2d8
PlanB <city7310@naver.com>
2017-09-01 01:19:12 +0900

    account.py의 계정 찾기 리소스 분할

--- Server/rest_modules/user/account.py
+++ Server/rest_modules/user/account.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource
 from flask import request
-import uuid
 
 from support.mysql import query
 
@@ -43,20 +42,3 @@ class SignIn(Resource):
                 return '', 200
             else:
                 return '', 204
-
-
-class FindPassword(Resource):
-    def post(self):
-        # 비밀번호 찾기
-        email = request.form['email']
-
-        res = query("SELECT * FROM account WHERE email='{0}'".format(email))
-        if res:
-            # id 존재함
-            new_pw = str(uuid.uuid4())[:8]
-            query("UPDATE account SET pw='{0}' WHERE email='{1}'".format(new_pw, email))
-
-            return new_pw, 200
-
-        else:
-            return '', 204
---
+++ Server/rest_modules/user/account_find.py
@@ -0,0 +1,53 @@
+from flask import request
+from flask_restful import Resource
+
+from support.mysql import query
+import uuid
+
+import smtplib
+from email.mime.text import MIMEText
+
+from_email = 'city7312@naver.com'
+
+
+def send_email(title, content, target):
+    message = MIMEText(content, _charset='utf-8')
+    message['subject'] = title
+    message['from'] = from_email
+    message['to'] = target
+
+    s = smtplib.SMTP('smtp.naver.com', 587)
+    s.starttls()
+    s.login('city7312', 'uursty199')
+    s.sendmail(from_email, target, message.as_string())
+    s.quit()
+
+
+class FindIdDemand(Resource):
+    def post(self):
+        # 아이디 찾기 : 인증 메일 전송
+        email = request.form['email']
+        code = str(uuid.uuid4())[:6]
+
+        query("DELETE FROM email_codes WHERE email='{0}'".format(email))
+        query("INSERT INTO email_codes VALUES('{0}', '{1}')".format(email, code))
+        send_email('[Bubble] 이메일 인증 코드입니다.', '인증 코드 : {0}'.format(code), email)
+
+        return '', 200
+
+
+class FindPassword(Resource):
+    def post(self):
+        # 비밀번호 찾기
+        email = request.form['email']
+
+        res = query("SELECT * FROM account WHERE email='{0}'".format(email))
+        if res:
+            # id 존재함
+            new_pw = str(uuid.uuid4())[:8]
+            query("UPDATE account SET pw='{0}' WHERE email='{1}'".format(new_pw, email))
+
+            return new_pw, 200
+
+        else:
+            return '', 204
