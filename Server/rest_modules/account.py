e7ed7ceb2453b353193cf27a850130e14b519ddb
PlanB <city7310@naver.com>
2017-08-31 19:51:00 +0900

    회원가입 구현 완료

--- Server/rest_modules/account.py
+++ Server/rest_modules/account.py
@@ -1,11 +1,24 @@
 from flask_restful import Resource
+from flask import request
+
 from support.mysql import query
 from support.mongo import db
 
 
 class Signup(Resource):
-    pass
+    def post(self):
+        _id = request.form['id']
+        pw = request.form['pw']
+        nickname = request.form['nickname']
+
+        res = query("SELECT * FROM account WHERE id='{0}'".format(_id))
+        if res:
+            return '', 204
+        else:
+            query("INSERT INTO account VALUES('{0}', '{1}', '{2}')".format(_id, pw, nickname))
+            return '', 201
 
 
 class SignIn(Resource):
-    pass
+    def post(self):
+        pass
