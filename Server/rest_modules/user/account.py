eadb44a1204245c16955920b2ef5ddfcd0c72a64
PlanB <city7310@naver.com>
2017-08-31 23:41:07 +0900

    비밀번호 찾기 구현
    uuid 기반 임시 비밀번호 발급

--- Server/rest_modules/user/account.py
+++ Server/rest_modules/user/account.py
@@ -1,12 +1,13 @@
 from flask_restful import Resource
 from flask import request
+import uuid
 
 from support.mysql import query
 
 
 class Signup(Resource):
-    # 회원가입
     def post(self):
+        # 회원가입
         _id = request.form['id']
         pw = request.form['pw']
         nickname = request.form['nickname']
@@ -20,13 +21,30 @@ class Signup(Resource):
 
 
 class SignIn(Resource):
-    # 로그인
     def post(self):
+        # 로그인
         _id = request.form['id']
         pw = request.form['pw']
 
         res = query("SELECT * FROM account WHERE id='{0}' AND pw='{1}'".format(_id, pw))
         if res:
             return '', 200
+        else:
+            return '', 204
+
+
+class FindPassword(Resource):
+    def post(self):
+        # 비밀번호 찾기
+        _id = request.form['id']
+
+        res = query("SELECT * FROM account WHERE id='{0}'".format(_id))
+        if res:
+            # id 존재함
+            new_pw = str(uuid.uuid4())[:8]
+            query("UPDATE account SET pw='{0}' WHERE id='{1}'".format(new_pw, _id))
+
+            return new_pw, 200
+
         else:
             return '', 204
