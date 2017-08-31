f6e7cdbe9f757b2c944b47d803181da743563243
PlanB <city7310@naver.com>
2017-09-01 03:15:39 +0900

    마이페이지 구현, main.py 재구성

--- Server/main.py
+++ Server/main.py
@@ -6,14 +6,20 @@ from rest_modules.in_app. market import Market, Download
 from rest_modules.in_app. preset import Preset, PresetDetail, UploadedPreset
 
 from rest_modules.user. user import MyPage, MyPageDetail
-from rest_modules.user. account import Signup, SignIn, FindPassword
+from rest_modules.user. account import Signup, SignIn
+from rest_modules.user .account_find import FindIdDemand, FindIdVerify, FindPwDemand, FindPwVerify, ChangePassword
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Signup, '/signup')
 api.add_resource(SignIn, '/signin')
-api.add_resource(FindPassword, '/find_password')
+
+api.add_resource(FindIdDemand, '/find_id/demand')
+api.add_resource(FindIdVerify, '/find_id/verify')
+api.add_resource(FindPwDemand, '/find_pw/demand')
+api.add_resource(FindPwVerify, '/find_pw/verify')
+api.add_resource(ChangePassword, '/change_password')
 
 api.add_resource(MyPage, '/mypage')
 api.add_resource(MyPageDetail, '/mypage/detail')
