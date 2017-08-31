0b49fe9f8c851a9406e307ffebc1afa71008497f
PlanB <city7310@naver.com>
2017-08-31 18:44:14 +0900

    Add preset.py

---
+++ Project/rest_modules/preset.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource
+from support.mysql import query
+from support.mongo import db
+
+
+class MyPreset(Resource):
+    pass
+
+
+class UploadedPreset(Resource):
+    pass
+
+
+class PresetDetail(Resource):
+    pass
+
+
+
