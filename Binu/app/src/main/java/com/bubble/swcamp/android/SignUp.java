9df84d7fe773748d88a6f57afe19dd0a8936b2df
geni429 <geni429@gmail.com>
2017-08-31 20:41:54 +0900

    signUp 통신 구현 완료

--- Binu/app/src/main/java/com/bubble/swcamp/android/SignUp.java
+++ Binu/app/src/main/java/com/bubble/swcamp/android/SignUp.java
@@ -3,16 +3,59 @@ package com.bubble.swcamp.android;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.bubble.swcamp.android.network.APIclient;
+import com.bubble.swcamp.android.network.APIinterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by geni on 2017. 8. 31..
  */
 
 public class SignUp extends AppCompatActivity {
+    private APIinterface apIinterface;
+    private Button signUpSubmitBtn;
+    private EditText inputId;
+    private EditText inputPw;
+    private EditText inputNickName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        apIinterface = APIclient.getClient().create(APIinterface.class);
+        signUpSubmitBtn = (Button)findViewById(R.id.signUpSubmit);
+        inputId = (EditText)findViewById(R.id.inputId);
+        inputPw = (EditText)findViewById(R.id.inputPw);
+        inputNickName = (EditText)findViewById(R.id.inputNickName);
+
+        signUpSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apIinterface.doSignUp(
+                        inputId.getText().toString(),
+                        inputPw.getText().toString(),
+                        inputNickName.getText().toString()).enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if(response.isSuccessful()){
+                            
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
 }
