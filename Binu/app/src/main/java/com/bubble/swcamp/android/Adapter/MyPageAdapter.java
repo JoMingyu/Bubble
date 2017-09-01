1b9dba8c8c8ed583d10bd2254a34df6e50ac93f5
geni429 <geni429@gmail.com>
2017-09-01 10:00:56 +0900

    mypage 구현 완료

--- Binu/app/src/main/java/com/bubble/swcamp/android/Adapter/MyPageAdapter.java
+++ Binu/app/src/main/java/com/bubble/swcamp/android/Adapter/MyPageAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bubble.swcamp.android.R;
@@ -39,21 +40,23 @@ public class MyPageAdapter extends RecyclerView.Adapter<MyPageAdapter.ViewHolder
                 holder.itemName.setTextColor(Color.parseColor("#66BB6A"));
                 holder.aboutItem.setText("현재까지 적립된 마일리지 내역입니다.");
                 holder.line.setBackgroundColor(Color.parseColor("#66BB6A"));
-                holder.bgColor.setBackgroundColor(Color.parseColor("#66BB6A"));
+                holder.bgColor.setBackgroundTintList(ContextCompat.getColorStateList(mContext, R.color.colorMyPageResA));
+                holder.icon.getLayoutParams().width = 70;
                 break;
             case 1:
                 holder.itemName.setText("누적 좋아요");
                 holder.itemName.setTextColor(Color.parseColor("#42A5F5"));
                 holder.aboutItem.setText("현재까지 누적된 좋아요 횟수입니다.");
                 holder.line.setBackgroundColor(Color.parseColor("#42A5F5"));
-                holder.bgColor.setBackgroundColor(Color.parseColor("#42A5F5"));
+                holder.bgColor.setBackgroundTintList(ContextCompat.getColorStateList(mContext, R.color.colorMyPageResB));
+//                holder.icon.setMaxWidth(50);
                 break;
             case 2:
                 holder.itemName.setText("프리셋 생성");
                 holder.itemName.setTextColor(Color.parseColor("#EF5350"));
                 holder.aboutItem.setText("현재까지 생성하신 프리셋 개수입니다.");
                 holder.line.setBackgroundColor(Color.parseColor("#EF5350"));
-                holder.bgColor.setBackgroundColor(Color.parseColor("#EF5350"));
+                holder.bgColor.setBackgroundTintList(ContextCompat.getColorStateList(mContext, R.color.colorMyPageResC));
                 break;
             case 3:
                 holder.itemName.setText("프리셋 업로드");
@@ -67,18 +70,18 @@ public class MyPageAdapter extends RecyclerView.Adapter<MyPageAdapter.ViewHolder
                 holder.itemName.setTextColor(Color.parseColor("#FF7043"));
                 holder.aboutItem.setText("현재까지 누적된 프리셋 다운로드 갯수입니다.");
                 holder.line.setBackgroundColor(Color.parseColor("#FF7043"));
-                holder.bgColor.setBackgroundColor(Color.parseColor("#FF7043"));
+                holder.bgColor.setBackgroundTintList(ContextCompat.getColorStateList(mContext, R.color.colorMyPageResE));
                 break;
             case 5:
                 holder.itemName.setText("내 프리셋 다운로드");
                 holder.itemName.setTextColor(Color.parseColor("#8D6E63"));
                 holder.aboutItem.setText("현재까지 누적된 내 프리셋 다운로드 수입니다.");
                 holder.line.setBackgroundColor(Color.parseColor("#8D6E63"));
-                holder.bgColor.setBackgroundColor(Color.parseColor("#8D6E63"));
+                holder.bgColor.setBackgroundTintList(ContextCompat.getColorStateList(mContext, R.color.colorMyPageResF));
                 break;
 
         }
-//        Glide.with(mContext).load(R.drawable.bg_my_page1+position).into(holder.bgItem);
+        Glide.with(mContext).load(R.drawable.ic_my_page1+position).into(holder.icon);
     }
 
     @Override
@@ -91,14 +94,16 @@ public class MyPageAdapter extends RecyclerView.Adapter<MyPageAdapter.ViewHolder
         TextView itemName;
         View line;
         TextView aboutItem;
-        LinearLayout bgColor;
+        RelativeLayout bgColor;
+        ImageView icon;
         public ViewHolder(View itemView) {
             super(itemView);
             this.value = (TextView) itemView.findViewById(R.id.value);
             this.itemName = (TextView) itemView.findViewById(R.id.item_name);
             this.line = itemView.findViewById(R.id.line);
             this.aboutItem = (TextView) itemView.findViewById(R.id.about_item);
-            this.bgColor = (LinearLayout) itemView.findViewById(R.id.bg_color);
+            this.bgColor = (RelativeLayout) itemView.findViewById(R.id.bg_color);
+            this.icon = (ImageView) itemView.findViewById(R.id.icon);
         }
     }
 
