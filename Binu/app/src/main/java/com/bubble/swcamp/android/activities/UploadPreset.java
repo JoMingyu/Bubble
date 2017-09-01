0c728f76ada1d89a0ebfc28da99d67b1bfd40810
geni429 <geni429@gmail.com>
2017-09-01 18:54:42 +0900

    upload preset 레이아웃 구성 완료

---
+++ Binu/app/src/main/java/com/bubble/swcamp/android/activities/UploadPreset.java
@@ -0,0 +1,19 @@
+package com.bubble.swcamp.android.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bubble.swcamp.android.R;
+
+/**
+ * Created by geni on 2017. 9. 1..
+ */
+
+public class UploadPreset extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload_preset);
+    }
+}
