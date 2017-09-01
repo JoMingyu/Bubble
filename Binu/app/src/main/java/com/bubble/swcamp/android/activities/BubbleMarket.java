86cde1e32402e5f0e232321ae4ae39f9d9a26ec1
geni429 <geni429@gmail.com>
2017-09-01 18:38:32 +0900

    bubble market 화면 구성 완료

---
+++ Binu/app/src/main/java/com/bubble/swcamp/android/activities/BubbleMarket.java
@@ -0,0 +1,28 @@
+package com.bubble.swcamp.android.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.bubble.swcamp.android.Adapter.BubbleMarketAdapter;
+import com.bubble.swcamp.android.R;
+
+/**
+ * Created by geni on 2017. 9. 1..
+ */
+
+public class BubbleMarket extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bubble_market);
+
+        recyclerView = (RecyclerView)findViewById(R.id.bubble_market_items);
+        recyclerView.setAdapter(new BubbleMarketAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+}
