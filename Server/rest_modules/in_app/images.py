12cebebbf6499d8fa19b3c4ffa70ee5038ffbe70
PlanB <city7310@naver.com>
2017-09-02 00:40:23 +0900

    images.py 추가

--- Server/rest_modules/in_app/images.py
+++ Server/rest_modules/in_app/images.py
@@ -15,9 +15,40 @@ class PresetImage(Resource):
         query("UPDATE preset SET image_name='{0}' WHERE preset_id={1}".format(filename, preset_id))
         f.save("images/{0}.png".format(filename))
 
-        return {'filename': filename}, 201
+        return '', 201
 
     def get(self):
         # 대표 이미지 다운로드
-        filename = request.args['filename']
-        return send_from_directory('images/', filename)
+        preset_id = request.args['preset_id']
+        filename = query("SELECT image_name FROM preset WHERE preset_id={0}".format(preset_id))[0]['image_name']
+
+        return send_from_directory('images/', '{0}.png'.format(filename))
+
+
+class ProfileImage(Resource):
+    def post(self):
+        # 프로필 사진 업로드
+        email = request.form['email']
+        f = request.files['image']
+        filename = uuid.uuid4()
+
+        if query("SELECT * FROM account WHERE email='{0}'".format(email)):
+            query("UPDATE account SET profile_image_name='{0}' WHERE email='{1}'".format(filename, email))
+        else:
+            query("UPDATE account_sns SET profile_image_name='{0}' WHERE email='{1}'".format(filename, email))
+
+        f.save("profile_images/{0}.png".format(filename))
+
+        return '', 201
+
+    def get(self):
+        # 대표 이미지 다운로드
+        email = request.args['email']
+
+        acc_data = query("SELECT * FROM account WHERE email='{0}'".format(email))
+        if not acc_data:
+            acc_data = query("SELECT * FROM account_sns WHERE email='{0}'".format(email))
+
+        filename = acc_data[0]['profile_image_name']
+
+        return send_from_directory('profile_images/', '{0}.png'.format(filename))
