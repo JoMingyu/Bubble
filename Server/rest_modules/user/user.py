7e024c9fbc87c67bdc3912de49a1b799ef38e061
PlanB <city7310@naver.com>
2017-09-01 14:20:03 +0900

    마이페이지 재구성

--- Server/rest_modules/user/user.py
+++ Server/rest_modules/user/user.py
@@ -9,11 +9,8 @@ class MyPage(Resource):
     def post(self):
         email = request.form['email']
 
-        preset_data = query("SELECT * FROM own_preset WHERE owner='{0}'".format(email))
+        preset_data = query("SELECT * FROM own_preset JOIN preset USING(preset_id) WHERE own_preset.owner='{0}'".format(email))
 
-        # type 1 : 내가 만들었는데 안 업로드
-        # type 2 : 내가 만들었는데 업로드
-        # type 3 : 다운로드
         created_preset = 0
         uploaded_preset = 0
         downloaded_preset = 0
@@ -21,22 +18,25 @@ class MyPage(Resource):
         download = 0
 
         for preset in preset_data:
-            if preset['type'] == 1:
-                created_preset += 1
-            elif preset['type'] == 2:
-                created_preset += 1
-                uploaded_preset += 1
+            if preset['poss'] == 1:
+                # 지가 만듬
+                if preset['uploaded'] == 1:
+                    # 지가 만들었고 업로드함
+                    created_preset += 1
+                    uploaded_preset += 1
+                    like += preset['like_count']
+                    download += preset['download_count']
+                else:
+                    # 지가 만들었는데 업로드 안함
+                    created_preset += 1
             else:
+                # 다운로드임
                 downloaded_preset += 1
-
-            if not preset['type'] == 3:
-                like += preset['like_count']
-                download += preset['download_count']
 
         acc_data = query("SELECT * FROM account WHERE email='{0}'".format(email))
         if not acc_data:
             acc_data = query("SELECT * FROM account_sns WHERE email='{0}'".format(email))
-
+            
         response = {
             'nickname': acc_data[0]['nickname'],
             'date': str(acc_data[0]['signup_date']),
