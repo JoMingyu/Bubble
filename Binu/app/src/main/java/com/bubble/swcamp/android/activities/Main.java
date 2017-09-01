b4a919d329a028254c9a1d7a4ad93c8b5fe24788
geni429 <geni429@gmail.com>
2017-09-01 21:25:04 +0900

    통신 인터페이스 완성

--- Binu/app/src/main/java/com/bubble/swcamp/android/activities/Main.java
+++ Binu/app/src/main/java/com/bubble/swcamp/android/activities/Main.java
@@ -2,18 +2,31 @@ package com.bubble.swcamp.android.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.bubble.swcamp.android.R;
+import com.bubble.swcamp.android.adapter.MainRankerPagerAdapter;
 
 /**
  * Created by geni on 2017. 8. 31..
  */
 
 public class Main extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        viewPager = (ViewPager)findViewById(R.id.ranker);
+        tabLayout = (TabLayout)findViewById(R.id.tabDots);
+        recyclerView = (RecyclerView)findViewById(R.id.filters);
+        tabLayout.setupWithViewPager(viewPager, true);
+        viewPager.setAdapter(new MainRankerPagerAdapter(getApplicationContext()));
     }
 }
